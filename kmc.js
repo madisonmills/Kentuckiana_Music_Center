1ba431e9935601aba46c6ac5d62b3e06d499038d
Madison Mills <madison.mills@lindsey.edu>
2020-07-29 20:18:20 -0400

    Made updates to navigation, resposiveness, images, boxes, etc. within the site

--- kmc.js
+++ kmc.js
@@ -38,4 +38,27 @@ function slideshow() {
 }
 var t = setInterval(slideshow, 3000);
 
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
+
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.querySelector(".myslides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1} 
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none"; 
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block"; 
+//     dots[slideIndex-1].className += " active";
+//   }
+
 
