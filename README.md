9762579a98c7cc88504b3e90ac3bbb35f6a4f25c
Madison Mills <madison.mills@lindsey.edu>
2020-07-31 01:53:08 -0400

    Made final changes to Readme, styling, Javascript, throughout site

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 
 ## Description 
 Hi!
-This is my attempt to improve the site: Kentuckianamusiccenter.com, a website of a local music store. 
+This is my attempt to improve the site: Kentuckianamusiccenter.com, a website of a local music store. From the time I started this project to now, I have seen their site change multiple times! There were several things that I wanted to fix such as a footer that collapsed and a lot of white space on their site. Neither site is perfect, but it has been a fun project and learning experience. 
 This site navigates you through the different components that are offered by the store.
 
 ## Main Features 
--HTML, CSS, and JavaScript were used. 
--Responsive navigation menu 
--HTML form with validation
+- HTML, CSS, and JavaScript were used. 
+- Responsive navigation menu 
+- HTML form with validation
 
 ## Feature Requirements
--Navigation menu that responds 
--Uses “:nth child” CSS to style a series of elements
--Form with input validation using Javascript 
--Flexbox used for styling navigation 
--Image carousel using Javascript 
+- Navigation menu that responds 
+- Uses “:nth child” CSS to style a series of elements
+- Form with input validation using Javascript 
+- Flexbox used for styling navigation 
+- Image carousel using Javascript 
--- kmc.js
+++ kmc.js
@@ -19,42 +19,27 @@ function validateForm() {
 
 function slideshow(x) {
     if (x == 1) {
-        document.querySelector("#pic-one").src="images/piano.jpg";
-        document.querySelector(".numbertext").innerHTML = x + "/ 3";
+        document.querySelector("#pic-one").src="images/violinlesson.jpg";
+        document.querySelector(".numbertext").innerHTML = x + " / 4";
+        document.querySelector(".imagetext").innerHTML = "Violin";
     }
     else if (x == 2)
     {
-        document.querySelector("#pic-one").src="images/cello.jpg";
-        document.querySelector(".numbertext").innerHTML = x + "/ 3";
+        document.querySelector("#pic-one").src="images/viola.jpg";
+        document.querySelector(".numbertext").innerHTML = x + " / 4";
+        document.querySelector(".imagetext").innerHTML = "Viola";
     }
     else if (x == 3) {
-        document.querySelector("#pic-one").src="images/violinlesson.jpg";
-        document.querySelector(".numbertext").innerHTML = x + "/ 3";
+        document.querySelector("#pic-one").src="images/cello.jpg";
+        document.querySelector(".numbertext").innerHTML = x + " / 4";
+        document.querySelector(".imagetext").innerHTML = "Cello";
+    }
+    else if (x == 4) {
+        document.querySelector("#pic-one").src="images/upright.jpg";
+        document.querySelector(".numbertext").innerHTML = x + " / 4";
+        document.querySelector(".imagetext").innerHTML = "Bass";
     }
 }
-var t = setInterval(slideshow, 3000);
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-//   }
-
-//   function showSlides(n) {
-//     var i;
-//     var slides = document.querySelector(".myslides");
-//     var dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) {slideIndex = 1} 
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none"; 
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex-1].style.display = "block"; 
-//     dots[slideIndex-1].className += " active";
-//   }
 
 
